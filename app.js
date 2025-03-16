9e751f075ebdaafcb81bf4d675fa5fddbc2fec35
joager paukoski <joagerp@gmail.com>
2025-03-16 15:20:46 -0300

    criado lista com validacao

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function exibirTexto (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function limparCampo(){
+    amigoAdicionado = document.querySelector('input');
+    amigoAdicionado.value = '';
+}
+
+function adicionarAmigo() {
+    let amigoAdicionado = document.querySelector('input').value;
+    if (amigoAdicionado == '') {
+        alert ('Por favor, insira algum nome.');
+    }
+    else {
+        listaAmigos.push(amigoAdicionado);
+        exibirTexto('ul', `${listaAmigos}`);
+    }
+    limparCampo();
+}
